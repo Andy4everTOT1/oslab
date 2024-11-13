86c0ed529e742d9bf295a3fb1a399ea0eb63e7a3
Carseny <Chenshenyao2@163.com>
2024-11-13 16:07:47 +0800

    3.8&3.9

--- lab3/lab3/arch/riscv/kernel/task_manager.c
+++ lab3/lab3/arch/riscv/kernel/task_manager.c
@@ -38,21 +38,80 @@ void task_init(void) {
     uint64_t task_addr = PHYSICAL_ADDR((uint64_t)&user_program_start) + i * PAGE_SIZE;
 
     // TODO: 完成用户栈的分配，并创建页表项，将用户栈映射到实际的物理地址
+
     // 1. 为用户栈分配物理页面，使用alloc_page函数
+    uint64_t* sp = alloc_page();
     // 2. 为用户进程分配根页表，使用alloc_page函数
+    uint64_t* pgtbl = alloc_page();
     // 3. 将task[i]->sscratch指定为虚拟空间下的栈地址，即0x1001000 + PAGE_SIZE（注意栈是从高地址到低地址使用的）
+    task[i]->sscratch = (uint64_t)0x1001000 + PAGE_SIZE;
     // 4. 正确设置task[i]->satp，注意设置ASID
+    task[i]->satp = (8L << 60) | ((uint64_t)i << 44) | ((uint64_t)pgtbl >> 12);
     // 5. 将用户栈映射到实际的物理地址，使用create_mapping函数
+    create_mapping(
+      pgtbl,
+      0x1001000,
+      sp,
+      4 << 10,
+      PTE_V | PTE_R | PTE_W | PTE_U
+    );
     // 6. 将用户程序映射到虚拟地址空间，使用create_mapping函数
+    create_mapping(
+      pgtbl,
+      0x1000000,
+      task_addr,
+      4 << 10,
+      PTE_V | PTE_R |PTE_X | PTE_W | PTE_U
+    );
     // 7. 将将虚拟地址 0xffffffc000000000 开始的 16 MB 空间映射到起始物理地址为 0x80000000 的 16MB 地址空间，注意此时 &rodata_start、... 得到的是虚拟地址还是物理地址？我们需要的是什么地址？
+    create_mapping(
+      pgtbl,
+      0xffffffc000000000,
+      0x80000000,
+      16 << 20,
+      PTE_V | PTE_R | PTE_W | PTE_X
+    );
     // 8. 对内核起始地址 0x80000000 的16MB空间做等值映射（将虚拟地址 0x80000000 开始的 16 MB 空间映射到起始物理地址为 0x80000000 的 16MB 空间），PTE_V | PTE_R | PTE_W | PTE_X 为映射的读写权限。
+    create_mapping(
+      pgtbl,
+      0x80000000,
+      0x80000000,
+      16 << 20,
+      PTE_V | PTE_R | PTE_W | PTE_X
+    );
     // 9. 修改对内核空间不同 section 所在页属性的设置，完成对不同section的保护，其中text段的权限为 r-x, rodata 段为 r--, 其他段为 rw-，注意上述两个映射都需要做保护。
-    // 10. 将必要的硬件地址（如 0x10000000 为起始地址的 UART ）进行等值映射 ( 可以映射连续 1MB 大小 )，无偏移，PTE_V | PTE_R | PTE_W 为映射的读写权限
-
-    
-    
-    
-
+    // text段权限为r-x
+    create_mapping(
+      pgtbl,
+      VIRTUAL_ADDR(&text_start),
+      PHYSICAL_ADDR(&text_start),
+      (&rodata_start - &text_start) * sizeof(uint64_t),
+      PTE_V | PTE_R | PTE_X
+    ); 
+    // rodata段权限为r--
+    create_mapping(
+      pgtbl,
+      VIRTUAL_ADDR(&rodata_start),
+      PHYSICAL_ADDR(&rodata_start),
+      (&data_start - &rodata_start) * sizeof(uint64_t),
+      PTE_V | PTE_R
+    );
+    // 其他段权限为rw-
+    create_mapping(
+      pgtbl,
+      VIRTUAL_ADDR(&data_start),
+      PHYSICAL_ADDR(&data_start),
+      (&_end - &data_start) * sizeof(uint64_t),
+      PTE_V | PTE_R | PTE_W
+    );
+    // 10. 将必要的硬件地址（如 0x10000000 为起始地址的 UART ）进行等值映射 ( 可以映射连续 1MB 大小 )，无偏移，PTE_V | PTE_R 为映射的读写权限
+    create_mapping(
+      pgtbl,
+      0x10000000,
+      0x10000000,
+      1 << 20,
+      PTE_V | PTE_R | PTE_W
+    );
 
     printf("[PID = %d] Process Create Successfully!\n", task[i]->pid);
   }
--- lab3/lab3/arch/riscv/kernel/vm.c
+++ lab3/lab3/arch/riscv/kernel/vm.c
@@ -45,7 +45,32 @@ void create_mapping(uint64_t *pgtbl, uint64_t va, uint64_t pa, uint64_t sz,
   // 8. 设置三级页表项的内容
 
   // TODO: 请完成你的代码
-  
+  uint64_t vpn, ppn;
+  uint64_t *PTE, *sub_tbl;
+  for (;sz > 0; sz -= PAGE_SIZE) {
+    sub_tbl = pgtbl;
+    for (uint64_t layer = 1; layer <= 3; layer++)
+    {
+      vpn = (va >> 12 >> (9 * (3 - layer))) & 0x1ff;
+      PTE = sub_tbl + vpn;  // 每项8 byte
+
+      if (!(*PTE & PTE_V)) {
+        if (layer == 3) {
+          ppn = pa >> 12;
+          *PTE = (ppn << 10) | PTE_V | perm;
+          break;
+        }
+        else {
+          ppn = alloc_page() >> 12;
+          *PTE = (ppn << 10) | PTE_V;
+        }
+      }
+
+      sub_tbl = *PTE >> 10 << 12;
+    }
+    va += PAGE_SIZE;
+    pa += PAGE_SIZE;
+  }
 }
 
 void paging_init() { 
@@ -60,6 +85,57 @@ void paging_init() {
   uint64_t *pgtbl = alloc_page();
   // TODO: 请完成你的代码
   
+  // 1. 创建内核的虚拟地址空间，调用 create_mapping 函数将虚拟地址 0xffffffc000000000 开始的 16 MB 空间映射到起始物理地址为 0x80000000 的 16MB 空间，PTE_V | PTE_R | PTE_W | PTE_X 为映射的读写权限。
+  create_mapping(
+    pgtbl, 
+    0xffffffc000000000, 
+    0x80000000, 
+    16 << 20, 
+    PTE_V | PTE_R | PTE_W | PTE_X
+  );
+  // 2. 对内核起始地址 0x80000000 的16MB空间做等值映射（将虚拟地址 0x80000000 开始的 16 MB 空间映射到起始物理地址为 0x80000000 的 16MB 空间），PTE_V | PTE_R | PTE_W | PTE_X 为映射的读写权限。
+  create_mapping(
+    pgtbl,
+    0x80000000,
+    0x80000000,
+    16 << 20,
+    PTE_V | PTE_R | PTE_W | PTE_X
+  );
+  // 3. 修改对内核空间不同 section 所在页属性的设置，完成对不同section的保护，其中text段的权限为 r-x, rodata 段为 r--, 其他段为 rw-，注意上述两个映射都需要做保护。
+  // text段权限为r-x
+  create_mapping(
+    pgtbl,
+    VIRTUAL_ADDR(&text_start),
+    PHYSICAL_ADDR(&text_start),
+    (&rodata_start - &text_start) * sizeof(uint64_t),
+    PTE_V | PTE_R | PTE_X
+  ); 
+  // rodata段权限为r--
+  create_mapping(
+    pgtbl,
+    VIRTUAL_ADDR(&rodata_start),
+    PHYSICAL_ADDR(&rodata_start),
+    (&data_start - &rodata_start) * sizeof(uint64_t),
+    PTE_V | PTE_R
+  );
+  // 其他段权限为rw-
+  create_mapping(
+    pgtbl,
+    VIRTUAL_ADDR(&data_start),
+    PHYSICAL_ADDR(&data_start),
+    (&_end - &data_start) * sizeof(uint64_t),
+    PTE_V | PTE_R | PTE_W
+  );
+
+
+  // 4. 将必要的硬件地址（如 0x10000000 为起始地址的 UART ）进行等值映射 ( 可以映射连续 1MB 大小 )，无偏移，PTE_V | PTE_R | PTE_W 为映射的读写权限
+  create_mapping(
+    pgtbl,
+    0x10000000,
+    0x10000000,
+    1 << 20,
+    PTE_V | PTE_R | PTE_W
+  );
 }
 
 
